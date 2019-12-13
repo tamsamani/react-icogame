7eb9e4ca2a1077ae6c9d6e3ae81a3829d3118983
tamsamani <tamsmoha@gmail.com>
2019-12-13 15:31:32 +0100

    3 files modified, 3 added

--- App.js
+++ App.js
@@ -1,17 +1,98 @@
 import React, { Component } from "react";
 
-class App extends Component {
+class Point {
+  displayName = "Point";
+  constructor(x = 0, y = 0) {
+    this.x = 0;
+    this.y = 0;
+  }
+
+  jump(x, y) {
+    !isNaN(x) && (this.x = x);
+    !isNaN(y) && (this.y = y);
+  }
+
+  move(dx, dy) {
+    !isNaN(dx) && (this.x += dx);
+    !isNaN(dy) && (this.y += dy);
+  }
+
+  toString() {
+    return `[${this.displayName}]{x:${this.x}, y:${this.y}}`;
+  }
+}
+
+class Camera extends Point {
   constructor() {
     super();
-    this.state = {
-      name: "React"
-    };
+    this.displayName = "Camera";
+  }
+}
+
+class App extends Component {
+  camera = new Camera();
+  state = {
+    cameraPosition: this.camera.toString()
+  };
+
+  handlerStore = {
+    isDown: false,
+    capture: [0, 0]
+  };
+
+  touchDownHandler(event) {
+    this.handlerStore.isDown = true;
+    this.handlerStore.capture = [event.clientX, event.clientY];
+  }
+  touchMoveHandler(event) {
+    if (this.handlerStore.isDown) {
+      // camera move handling
+      let [cX, cY] = this.handlerStore.capture;
+
+      // compare variation
+      let dx = event.clientX - cX;
+      let dy = event.clientY - cY;
+
+      this.camera.move(dx, dy);
+
+      // set new capture
+      this.handlerStore.capture = [event.clientX, event.clientY];
+
+      this.setState({
+        cameraPosition: this.camera.toString()
+      });
+    }
+  }
+  touchUpHandler(event) {
+    this.handlerStore.isDown = false;
+    this.handlerStore.capture = [0, 0];
+
+    // set new Position
+    this.setState({
+      cameraPosition: this.camera.toString()
+    });
   }
 
   render() {
+    let { cameraPosition } = this.state;
     return (
-      <div>
-        <p>Welcom Ico Game {this.state.name}</p>
+      <div className="play-ground">
+        <div
+          className="board-control"
+          onPointerDown={e => this.touchDownHandler(e)}
+          onPointerUp={e => this.touchUpHandler(e)}
+          onPointerMove={e => this.touchMoveHandler(e)}
+        />
+        <div
+          className="camera"
+          style={{
+            left: this.camera.x,
+            top: this.camera.y
+          }}
+        >
+          {cameraPosition.toString()}
+        </div>
+        <div>Player </div>
       </div>
     );
   }
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# react-cvyeuq
+# react-icongame
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-cvyeuq)
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-icogame)
+
+### Idea & Represenation
+---
+this game based to emoji/icons of [Unicode 12.0][1] 
+
+
+
+[1]: https://unicode.org/
